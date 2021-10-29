d9428f680b21b7f768061ba11b94b5fcc1c1d9cd
SuperMong <1393731005@qq.com>
2021-10-29 13:39:31 +0800

    feat: add patch 'open_copied_url'

--- config.h
+++ config.h
@@ -206,6 +206,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ MODKEY,               XK_o,           opencopied,     {.v = "chromium"} },
 	{ ControlMask,					XK_k,						kscrollup,      {.i =  1} },
 	{ ControlMask,					XK_j,						kscrolldown,    {.i =  1} },
 	{ MODKEY|ControlMask,		XK_k,						kscrollup,      {.i = -1} },
